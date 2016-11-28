2449a6fbca68e926ccda45e7a4ad6fa548b33787
tessy <tessy.joseph@chillr.in>
2016-11-28 12:07:29 +0530

    Add User blueprint

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    from user.views import user_app
+    app.register_blueprint(user_app)
     return app
-
-
